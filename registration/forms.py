ed4d91d287c1768770177224c571eabbfbc69ed8
fady2019 <fadyemadfawzygoher258456@gmail.com>
2021-07-16 16:53:18 +0200

    Done

---
+++ registration/forms.py
@@ -0,0 +1,94 @@
+from django.contrib.auth.forms import PasswordChangeForm, UserChangeForm
+from django import forms
+from django.contrib.auth import authenticate, get_user_model
+
+from .models import User as UserModel
+
+User = get_user_model()
+
+
+class UserSignupForm(forms.ModelForm):
+    first_name = forms.CharField(label='First Name')
+    last_name = forms.CharField(label='Last Name')
+    username = forms.CharField(label="Username")
+    password = forms.CharField(widget=forms.PasswordInput, label="Password")
+    confirmPassword = forms.CharField(
+        widget=forms.PasswordInput, label="Confirm Password")
+
+    class Meta:
+        model = User
+        fields = [
+            'first_name',
+            'last_name',
+            'username',
+            'password',
+            'confirmPassword'
+        ]
+
+    def clean(self, *args, **kwargs):
+        username = self.cleaned_data.get('username')
+        password = self.cleaned_data.get('password')
+        confirmPassword = self.cleaned_data.get('confirmPassword')
+
+        doesUsernameExist = User.objects.filter(username=username).exists()
+
+        if doesUsernameExist:
+            raise forms.ValidationError('This username is already exist !')
+        if password != confirmPassword:
+            raise forms.ValidationError('These passwords are not matched !')
+
+        return super(UserSignupForm, self).clean(*args, **kwargs)
+
+
+class UserUpdateInfoForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = [
+            'first_name',
+            'last_name',
+            'username'
+        ]
+
+
+class UserUpdatePassForm(PasswordChangeForm):
+    def clean(self):
+        return super(UserUpdatePassForm, self).clean()
+
+
+class UserLoginForm(forms.Form):
+    username = forms.CharField(label='Username')
+    password = forms.CharField(widget=forms.PasswordInput, label='Password')
+    userType = forms.CharField(
+        disabled=True, label='User Type', widget=forms.HiddenInput)
+
+    class Meta:
+        model = User
+        fields = [
+            'username',
+            'password',
+            'userType'
+        ]
+
+    def clean(self, *args, **kwargs):
+        username = self.cleaned_data.get('username')
+        password = self.cleaned_data.get('password')
+        userType = self.cleaned_data.get('userType')
+
+        if username and password:
+            user = authenticate(username=username, password=password)
+
+            if not user:
+                raise forms.ValidationError(
+                    'This user does not exist or the password is incorrect !')
+            if not user.is_active:
+                raise forms.ValidationError('This user does not exist !')
+
+            userQuerySet = UserModel.objects.filter(user=user).values()
+            userData = [row for row in userQuerySet]
+            storedUserType = userData[0]['userType']
+
+            if userType != storedUserType:
+                raise forms.ValidationError(
+                    'There is no ' + userType + ' with this username !')
+
+        return super(UserLoginForm, self).clean(*args, **kwargs)
